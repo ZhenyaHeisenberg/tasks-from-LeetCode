d68ccb899a38a961e44e309e2c6cfe48b19d05ae
Roman Shashkov <em.krem08@gmail.com>
2025-11-23 00:00:24 +0300

    fix

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -1,21 +1,22 @@
 class Solution:
-    def threeSum():
-        
-        nums = [1, -2, 1]
-        
-        
-        nums.sort()
-        result = set()
-        
-        for i in range(len(nums)):
-            seen = set()
-            for j in range(i + 1, len(nums)):
-                complement = -nums[i] - nums[j]
-                if complement in seen:
-                    result.add((nums[i], complement, nums[j]))
-                seen.add(nums[j]) # добавляем элемент, который уже не попадет в i или j 
-                """"""
-        
-        return [list(triplet) for triplet in result]
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        s = {}
 
-    print(threeSum())
+        for i in range(len(nums)):
+            s[nums[i]] = 0
+
+
+
+        answer = []
+
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if -1*(nums[i]+nums[j]) in s:
+                    if i != j and i != nums.index(-1*(nums[i]+nums[j])) and nums.index(-1*(nums[i]+nums[j])) != j:
+
+                        p = [nums[i], nums[j], -1*(nums[i]+nums[j])]
+                        p.sort()
+                        if p not in answer:
+                            answer.append(p)
+
+        return answer
